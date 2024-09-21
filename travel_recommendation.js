11e97ae3252a6d4e9547337f16e8a66668775f7a
daviftorress <davidf.torress@gmail.com>
2024-09-20 20:32:53 -0400

    final proyect- time 1.0

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,5 +1,29 @@
 let data = [];
+/*
+const timeZones = {
+    "Australia": "Australia/Sydney",
+    "Japan": "Asia/Tokyo",
+    "Brazil": "America/Sao_Paulo",
+    "India": "Asia/Kolkata",
+    "USA": "America/New_York"
+};
+const currentlyTime = new Date().toLocaleTimeString('en-US', options);
 
+//Funcion Hora local
+function getOptions(country) {
+    const timeZone = timeZones[country];
+    if (!timeZone) {
+        throw new Error(`Zona horaria no encontrada para el país: ${country}`);
+    }
+    return {
+        timeZone: timeZone,
+        hour12: true,
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+    };
+}
+*/
 // Cargar datos del archivo JSON
 fetch('travel_recommendation_api.json')
     .then(response => {
@@ -19,48 +43,65 @@ fetch('travel_recommendation_api.json')
 // funciones
 function search() {
     const input = document.getElementById('conditionInput').value.toLowerCase();
-    console.log(input);
+   // const options = getOptions(input);
+
     const resultsDiv = document.getElementById('results');
     resultsDiv.innerHTML = '';
     const filteredResults = [];
-
-   // Buscar en cada categoría
-    data.forEach(category => {
-        if (category.countries) {
-            category.countries.forEach(country => {
-                if (country.name.toLowerCase().includes(input)) {
-                    country.cities.forEach(city => {
-                        filteredResults.push(city);
-                    });
-                }
-            });
-        }
-        if (category.temples) {
-            category.temples.forEach(temple => {
-                if (temple.name.toLowerCase().includes(input)) {
-                    filteredResults.push(temple);
-                }
-            });
-        }
-        if (category.beaches) {
+    if(input.toLowerCase().includes('beach')){
+        data.forEach(category => {  
             category.beaches.forEach(beach => {
-                if (beach.name.toLowerCase().includes(input)) {
-                    filteredResults.push(beach);
-                }
+                filteredResults.push(beach);   
             });
-        }
-    });
+        });
+    }else if(input.toLowerCase().includes('temple')){
+        data.forEach(category => { 
+            category.temples.forEach(temple => {
+                filteredResults.push(temple);   
+            });
+        });
+    }else {
+        // Buscar en cada categoría
+        data.forEach(category => {
+            if (category.countries) {
+                category.countries.forEach(country => {
+                    if (country.name.toLowerCase().includes(input)) {
+                        country.cities.forEach(city => {
+                            filteredResults.push(city);
+                        });
+                    }
+                });
+            }
+            if (category.temples) {
+                category.temples.forEach(temple => {
+                    if (temple.name.toLowerCase().includes(input)) {
+                        filteredResults.push(temple);
+                    }
+                });
+            }
+            if (category.beaches) {
+                category.beaches.forEach(beach => {
+                    if (beach.name.toLowerCase().includes(input)) {
+                        filteredResults.push(beach);
+                    }
+                });
+            }
+        });
 
-    // Mostrar resultados
-    filteredResults.forEach(item => {
-        const itemDiv = document.createElement('div');
-        itemDiv.innerHTML = `
-            <h3>${item.name}</h3>
-            <img src="${item.imageUrl}" alt="${item.name}">
-            <p>${item.description}</p>
-        `;
-        resultsDiv.appendChild(itemDiv);
-    });
+    }
+  // Mostrar resultados
+filteredResults.forEach(item => {
+    const itemDiv = document.createElement('div');
+    itemDiv.innerHTML = `
+        <h3>${item.name}</h3>
+        <img src="${item.imageUrl}" alt="${item.name}">
+        <p>${item.description}</p><br>
+        <br><button>Visit</button>
+
+    `;
+    resultsDiv.appendChild(itemDiv);
+});  
+  
 }
 
 function clearSearch() {
